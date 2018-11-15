a83c3c7cee1bf8c2d408574bd2fe9aa9a0c4258b
damy <damy@N80239904-2.local>
2018-11-15 17:14:40 +0800

    set demo

--- vue-demo/src/router.js
+++ vue-demo/src/router.js
@@ -22,13 +22,12 @@ export default new Router({
         import(/* webpackChunkName: "about" */ "./views/Slot.vue")
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/set",
+      name: "set",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: () => import(/* webpackChunkName: "about" */ "./views/Set.vue")
     }
   ]
 });
---
+++ vue-demo/src/views/Set.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>{{list.name}}</div>
+    <div>{{list.family}}</div>
+    <child></child>
+  </div>
+</template>
+
+<script>
+import child from "./SetChild";
+export default {
+  data() {
+    return {
+      list: {}
+    };
+  },
+  mounted() {
+    // this.list.name = "zhidan";
+    // this.list.family = "xu";
+    this.$set(this.list, "name", "zhidan");
+    this.$set(this.list, "family", "xu");
+  },
+  components: {
+    child
+  }
+};
+</script>
+
+<style scoped>
+</style>
